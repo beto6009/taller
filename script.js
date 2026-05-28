81b88c585d3de24b53bf9b633aec4739040229f5
beto6009 <beto6009@gmail.com>
2026-05-28 13:08:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -209,8 +209,8 @@ async function generarPDF() {
 
         pdf.setFontSize(10);
 
-        pdf.text(subtotal.toString(), 182, 228.45);
-        pdf.text(iva.toString(), 182, 235.50);
+        pdf.text(subtotal.toString(), 182, 228.65);
+        pdf.text(iva.toString(), 182, 235.75);
 
         pdf.setFont("helvetica", "bold");
         pdf.setFontSize(10);
