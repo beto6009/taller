e537d2e503e02b0769db7ad080a3d9d62cc18e5c
beto6009 <beto6009@gmail.com>
2026-05-28 13:03:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -209,13 +209,13 @@ async function generarPDF() {
 
         pdf.setFontSize(10);
 
-        pdf.text(subtotal.toString(), 182, 227.50);
-        pdf.text(iva.toString(), 182, 234.50);
+        pdf.text(subtotal.toString(), 182, 227.60);
+        pdf.text(iva.toString(), 182, 234.60);
 
         pdf.setFont("helvetica", "bold");
         pdf.setFontSize(10);
 
-        pdf.text(total.toString(), 182, 243.50);
+        pdf.text(total.toString(), 182, 243.60);
         pdf.text("Ivan Salinas Morales".toString(), 152.93, 286.32);
 
 
